a24a20a90b0da611f1c9564d28d2cffc363b108e
PHP <php3droso@gmail.com>
2023-06-13 17:45:12 -0300

    Todo-list componentizado

---
+++ src/components/Formulario.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['cadastrarTarefa','tarefaTemp', 'trocarFiltro','editTarefaTemp']);
+</script>
+<template>
+    <form @submit.prevent="props.cadastrarTarefa">
+        <div class="row">
+            <div class="col-8">
+                <input :value="props.tarefaTemp" @change="props.editTarefaTemp" required
+                    type="text" placeholder="Digite a tarefa" class="form-control">
+            </div>
+            <div class="col-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                    <option value="todas">Todas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
---
+++ src/components/ListaTarefa.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const props = defineProps(['tarefas','filtro']);
+</script>
+<template>
+    <ul v-if="props.tarefas.length > 0" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+                :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">{{ tarefa.titulo }}</label>
+        </li>
+    </ul>
+    <h4 v-else class="mt-4">Não existem tarefas {{ filtro }}!!!</h4>
+</template>
+<style scoped>
+    .done {
+        text-decoration: line-through;
+    }
+</style>
